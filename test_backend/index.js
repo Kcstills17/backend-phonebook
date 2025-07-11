84f264832c3bbdfe4f5c167d15c40ad40d7c49f0
Zane <zane17@MacBookPro.attlocal.net>
2025-07-11 08:05:29 -0400

    changed some port settings for deployment

--- test_backend/index.js
+++ test_backend/index.js
@@ -86,7 +86,8 @@ app.delete('/api/notes/:id', (request, response) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
-app.listen(PORT)
-console.log(`server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
